532fce2f1f7311d658f5660f24113befd4134b35
Denis Kulik <kulikdenis1996x@gmail.com>
2024-04-29 15:23:26 +0700

    feat: add app header template

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import AppHeader from '@/components/AppHeader.vue'
+</script>
 
 <template>
-  <div></div>
+  <div class="w-4/5 h-screen m-auto mt-14 bg-white rounded-xl shadow-xl">
+    <AppHeader />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="flex justify-between p-8 border-b border-slate-300">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="Logo" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+        <p class="text-slate-400">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+
+    <nav class="flex items-center">
+      <ul class="flex items-center gap-10">
+        <li class="flex items-center gap-3 text-gray-500 cursor-pointer hover:text-black">
+          <img src="/cart.svg" alt="Cart" />
+          <b>1205 руб.</b>
+        </li>
+        <li class="flex items-center gap-3 text-gray-500 cursor-pointer hover:text-black">
+          <img src="/heart.svg" alt="Cart" />
+          <span>Закладки</span>
+        </li>
+        <li class="flex items-center gap-3 text-gray-500 cursor-pointer hover:text-black">
+          <img src="/profile.svg" alt="Cart" />
+          <span>Профиль</span>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
