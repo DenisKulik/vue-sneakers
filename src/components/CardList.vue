455cb5f9da686aad7f2d7eadc9f7dfd94d4278a6
Denis Kulik <kulikdenis1996x@gmail.com>
2024-04-30 09:46:33 +0700

    feat: add props for the product card

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,13 +1,25 @@
 <script setup>
 import ProductCard from '@/components/ProductCard.vue'
+
+const onAddProductToCart = () => {
+  console.log('onAddProductToCart')
+}
+
+const onAddProductToFavorite = () => {
+  console.log('onAddProductToFavorite')
+}
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
+    <ProductCard
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      image-url="/vue-sneakers/sneakers/sneakers-1.jpg"
+      :price="12999"
+      :is-favorite="true"
+      :is-added="true"
+      @onClickAdd="onAddProductToCart"
+      @onClickFavorite="onAddProductToFavorite"
+    />
   </div>
 </template>
--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,16 +1,36 @@
+<script setup>
+defineProps({
+  title: String,
+  imageUrl: String,
+  price: Number,
+  isFavorite: Boolean,
+  isAdded: Boolean
+})
+</script>
+
 <template>
   <div
     class="relative p-8 cursor-pointer bg-white border border-slate-100 rounded-3xl hover:-translate-y-2 hover:shadow-xl transition"
   >
-    <img src="/like-1.svg" class="absolute top-8 left-8" alt="Like 1" />
-    <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
-    <p class="mt-2">Мужские Кроссовки Nike Blazer Mid Suede</p>
+    <img
+      class="absolute top-8 left-8"
+      :src="isFavorite ? '/vue-sneakers/like-2.svg' : '/vue-sneakers/like-1.svg'"
+      alt="Like"
+      @click.stop="$emit('onClickFavorite')"
+    />
+
+    <img :src="imageUrl" alt="Sneaker" />
+    <p class="mt-2">{{ title }}</p>
     <div class="flex justify-between mt-5">
       <div class="flex flex-col">
         <span class="text-slate-400">Цена:</span>
-        <b>12 999 руб.</b>
+        <b>{{ price }} руб.</b>
       </div>
-      <img src="/plus.svg" alt="Add to cart" />
+      <img
+        :src="isAdded ? '/vue-sneakers/checked.svg' : '/vue-sneakers/plus.svg'"
+        alt="Add to cart"
+        @click.stop="$emit('onClickAdd')"
+      />
     </div>
   </div>
 </template>
