8555cdd79af6a062de9482864c9cd0b29e48c616
Denis Kulik <kulikdenis1996x@gmail.com>
2024-05-12 12:33:44 +0700

    chore: configure auto-animate to the project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import App from './App.vue'
 import './style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(autoAnimatePlugin)
+app.mount('#app')
