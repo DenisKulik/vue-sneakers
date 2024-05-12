346637fc063fba9d4d57c259c03f9490c53e91d9
Denis Kulik <kulikdenis1996x@gmail.com>
2024-05-12 14:12:08 +0700

    chore: configure pinia to the project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import App from './App.vue'
 import './style.css'
 
 const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
 app.use(autoAnimatePlugin)
 app.mount('#app')
